4131b319abede0fa54f11ec4f3e0b3b6902c6ef7
Tamer Sali <tamerrsali@gmail.com>
2020-12-01 23:08:07 +0200

    app navigate section

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,8 +3,8 @@ import { Button } from '@material-ui/core'
 
 export default function Navigation() {
     return (
-        <nav className="sign">
-            <Button size="large" variant="outlined">Sign Out</Button>
+        <nav className="navbar">
+            <Button size="small" variant="outlined">Sign Out</Button>
         </nav>
     )
 }
