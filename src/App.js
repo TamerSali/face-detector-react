ad3f9ea0a992693ce46be38374927bec67f36c44
Tamer Sali <tamerrsali@gmail.com>
2020-12-01 23:05:30 +0200

    app child components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+/**
+ * External Dependencies.
+ */
 import React from 'react'
-
+/**
+ * Internal Dependencies.
+ */
 import Navigation from './components/Navigation'
 import Logo from './components/Logo'
-
+import ImageLinkForm from './components/ImageLinkForm'
 
 function App() {
 	return (
 		<div className="App">
-			<Navigation />
-			<Logo />
-			{/* <ImageLinkForm />
-			<FaceRecognition /> */}
+			<header className="app-header">
+				<Logo />
+				<Navigation />
+			</header>
+			<main>
+				<ImageLinkForm />
+			</main>
 		</div>
 	);
 }
