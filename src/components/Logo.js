2d5f333c7953e2bf081a57cbafb01e415f632051
Tamer Sali <tamerrsali@gmail.com>
2020-12-02 15:24:40 +0200

    moved className app-logo to header tag

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -10,8 +10,8 @@ import Tilt from 'react-tilt'
 
 export default function Logo() {
 	return (
-		<header>
- 		<Tilt className="Tilt app-logo" options={{ max: 70, scale: 1.0 }} style={{ height: 100, width: 100 }} >
+		<header className="app-logo">
+ 		<Tilt className="Tilt" options={{ max: 70, scale: 1.0 }} style={{ height: 100, width: 100 }} >
 			<div className="Tilt-inner">
 				<img src={logo} alt="app-logo" />
 			</div>
