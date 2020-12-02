1fb90547bf249a2b9c2d46ba449755b03af5a50e
Tamer Sali <tamerrsali@gmail.com>
2020-12-02 19:43:53 +0200

    display image and draw a box over the face

--- src/components/FaceRecognition.js
+++ src/components/FaceRecognition.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
-export default function FaceRecognition() {
-    return (
-        <div>
-        </div>
-    )
+export default function FaceRecognition({ imageURL, drawBox }) {
+	const { leftCol, topRow, rightCol, bottomRow } = drawBox;
+
+	return (
+		<div className="image-with-faces">
+			{imageURL && (
+				<section>
+					<img
+						id="input-image"
+						src={imageURL}
+						width="400"
+						height="auto"
+						alt="face-detect"
+					/>
+					<div
+						className="bounding-box"
+						style={{ top: topRow, left: leftCol, right: rightCol, bottom: bottomRow }}
+					/>
+				</section>
+			)}
+		</div>
+	)
 }
