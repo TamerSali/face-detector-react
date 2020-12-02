2c5d51c04a176eafb6630825e0c1a636cd2c4d83
Tamer Sali <tamerrsali@gmail.com>
2020-12-03 00:55:45 +0200

    moved logo content  from Logo.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,25 @@
+/**
+ * External Dependencies.
+ */
 import React from 'react'
+/**
+ * Internal Dependencies.
+ */
+import logo from '../assets/logo.svg'
+import Tilt from 'react-tilt'
 import { Button } from '@material-ui/core'
 
 export default function Navigation() {
     return (
         <nav className="navbar">
-            <Button size="small" variant="outlined">Sign Out</Button>
+            <div className="app-logo">
+                <Tilt className="Tilt" options={{ max: 70, scale: 1.0 }} style={{ height: 100, width: 100 }} >
+                    <div className="Tilt-inner">
+                        <img src={logo} alt="app-logo" />
+                    </div>
+                </Tilt>
+            </div>
+            <Button size="small" variant="outlined">Sign In</Button>
         </nav>
     )
 }
