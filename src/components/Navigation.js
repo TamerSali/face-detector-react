92cb00b1a1a6b904835c009976785138afb891f1
Tamer Sali <tamerrsali@gmail.com>
2020-12-03 20:36:41 +0200

    renamed button

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,16 +10,16 @@ import Tilt from 'react-tilt'
 import { Button } from '@material-ui/core'
 
 export default function Navigation() {
-    return (
-        <nav className="navbar">
-            <div className="app-logo">
-                <Tilt className="Tilt" options={{ max: 70, scale: 1.0 }} style={{ height: 100, width: 100 }} >
-                    <div className="Tilt-inner">
-                        <img src={logo} alt="app-logo" />
-                    </div>
-                </Tilt>
-            </div>
-            <Button size="small" variant="outlined">Sign In</Button>
-        </nav>
-    )
+	return (
+		<nav className="navbar">
+			<div className="app-logo">
+				<Tilt className="Tilt" options={{ max: 70, scale: 1.0 }} style={{ height: 100, width: 100 }} >
+					<div className="Tilt-inner">
+						<img src={logo} alt="app-logo" />
+					</div>
+				</Tilt>
+			</div>
+			 <Button size="small" variant="outlined">Log Out</Button>
+		</nav>
+	)
 }
